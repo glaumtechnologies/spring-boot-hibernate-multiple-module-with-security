2993b4ae6824a8da7bdde9fb1d2f214396f8f12b
vijayanasokan <vijayan.a@kgfsl.com>
2020-09-24 11:29:20 +0530

    Add files via upload

---
+++ login/src/main/java/com/glaum/login/entity/permission.java
@@ -0,0 +1,51 @@
+package com.glaum.login.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "permission")
+public class permission {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
+	private int id;
+	
+	@Column(name="bit")
+	private int bit;
+	
+	@Column(name="name")
+	private String name;
+	
+	public int getid()
+	{
+		return id;
+	}
+	
+	public void setid(int id)
+	{
+		this.id=id;
+	}
+	
+	public int getbit()
+	{
+		return bit;
+	}
+	public void setbit(int bit) {
+		this.bit=bit;
+	}
+	public String getname()
+	{
+		return name;
+	}
+	public void setname(String name)
+	{
+		this.name=name;
+	}
+	
+}
