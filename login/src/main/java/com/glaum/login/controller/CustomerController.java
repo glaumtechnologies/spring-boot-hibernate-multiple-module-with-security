7b9a6af7d87ed854ead87cdc98c0db3973e5550c
vijayanasokan <vijayan.a@kgfsl.com>
2020-09-24 15:35:36 +0530

    Update CustomerController.java
    test method for userpermission

--- login/src/main/java/com/glaum/login/controller/CustomerController.java
+++ login/src/main/java/com/glaum/login/controller/CustomerController.java
@@ -3,17 +3,22 @@ package com.glaum.login.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.glaum.login.configuration.Authorized;
 import com.glaum.login.entity.permission;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -44,6 +49,25 @@ public class CustomerController {
     public String customerdel(HttpSession httpSession,HttpServletRequest request,HttpServletResponse res) throws IOException {
     	
         return "admin_customer";
+    }
+    
+    @RequestMapping(value={"/getuserpermission"}, method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+    @Authorized(keys = {"quote_delete","quote_read","quote_create"})
+    @ResponseBody
+    public Map<String, Integer> fetchuserpermission(HttpSession httpSession,HttpServletRequest request,HttpServletResponse res) throws IOException {
+    	Map<String,Integer> mapofper= new HashMap<String, Integer>();
+    	
+    	for (Field f : httpSession.getClass().getDeclaredFields()) {
+            f.setAccessible(true);
+            Object o;
+            try {
+               HttpSession s= (HttpSession) f.get(httpSession);	              
+               mapofper= (Map<String, Integer>) s.getAttribute("userpermissiondetails");
+            } catch (Exception e) {
+                o = e;
+            }	           
+        }
+        return mapofper;
     }
     
    
