f73bc2ab02ade27993357a0145a54b2db31fd1cb
vijayanasokan <vjasokan37@gmail.com>
2020-09-25 13:56:57 +0530

    Update AuthAspect.java

--- login/src/main/java/com/glaum/login/configuration/AuthAspect.java
+++ login/src/main/java/com/glaum/login/configuration/AuthAspect.java
@@ -1,9 +1,7 @@
 package com.glaum.login.configuration;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,10 +12,12 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-import com.glaum.login.entity.permission;
+import com.glaum.login.entity.Permission;
 import com.google.common.collect.Lists;
 
 import groovy.util.logging.Slf4j;
@@ -27,66 +27,46 @@ import groovy.util.logging.Slf4j;
 @Component
 public class AuthAspect {
 
+	@Before("@annotation(Authorized) && args() ")
+	public static void before(JoinPoint jointpoint) {
 
+		boolean authallowuser = false;
+		int roleid = 0;
+		List<Permission> lstofpermission = Lists.newArrayList();
+		int bit = -1;
 
-   
-  @Before("@annotation(Authorized) && args(httpSession,request,res) ")
-   public static void before(JoinPoint jt,HttpSession httpSession,HttpServletRequest request,HttpServletResponse res){
-	 
-	  boolean authper=false;
-	  String roleid=""; 
-      List<permission> lstobj=Lists.newArrayList(); 
-      int bit=-1;
-      
-      MethodSignature signature = (MethodSignature) jt.getSignature();
-      Method method = signature.getMethod();
-      
-      Authorized objauth=method.getAnnotation(Authorized.class);
-      String[] arrofpermission = objauth.keys();
-      System.out.println("printing aspect val"+((Arrays.toString(arrofpermission))));   
-      
-      
+		MethodSignature signature = (MethodSignature) jointpoint.getSignature();
+		Method method = signature.getMethod();
+		Authorized objauthorized = method.getAnnotation(Authorized.class);
+		String[] arrayofpermission = objauthorized.keys();
 
-      
-      for (Field f : httpSession.getClass().getDeclaredFields()) {
-          f.setAccessible(true);
-          Object o;
-          try {
-             HttpSession s= (HttpSession) f.get(httpSession);	              
-             lstobj= (List<com.glaum.login.entity.permission>) s.getAttribute("permissionval");
-             roleid=s.getAttribute("roleid").toString();
-          } catch (Exception e) {
-              o = e;
-          }	           
-      }
-     // System.out.println("role"+roleid);
-      for(String s:arrofpermission)
-      {
-      for (permission per : lstobj) {
-          if (per.getname().equalsIgnoreCase(s)) {
-          	bit=per.getbit();          	
-          	if((Integer.parseInt(roleid) & bit) == bit)
-          	{
-          		authper=true;
-          	}
-          }
-      }
-      }
-//	System.out.println((Integer.parseInt(roleid) & bit) == bit);
-//      authper=((Integer.parseInt(roleid) & bit) == bit);
-      if(!authper)
-		try {
-			res.sendRedirect(request.getContextPath() + "/accessdenied");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+		HttpServletRequest httprequest = ((ServletRequestAttributes) requestAttributes).getRequest();
+		HttpServletResponse httpresponse = ((ServletRequestAttributes) requestAttributes).getResponse();
+		HttpSession session = httprequest.getSession();
+
+		roleid = (int) session.getAttribute("roleid");
+		lstofpermission = (List<com.glaum.login.entity.Permission>) session.getAttribute("permissionval");
+
+		for (String permissionreq : arrayofpermission) {
+			for (Permission availablepermission : lstofpermission) {
+				if (availablepermission.getname().equalsIgnoreCase(permissionreq)) {
+					bit = availablepermission.getbit();
+					if ((roleid & bit) == bit) {
+						authallowuser = true;
+					}
+				}
+			}
 		}
 
-   } 
+		if (!authallowuser)
+			try {
+				httpresponse.sendRedirect(httprequest.getContextPath() + "/accessdenied");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+	}
 //   
 
-
-
-
-   
 }
